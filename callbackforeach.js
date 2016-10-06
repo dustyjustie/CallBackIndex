369ab2c2bb4266a21ad3067ea1716af1abea0bdd
Justin Tsang <justin.b.tsang@gmail.com>
2016-10-06 20:48:46 +0000

    added for each loop in lieu of for

---
+++ callbackforeach.js
@@ -0,0 +1,15 @@
+function findWaldo(arr, found) {
+arr.forEach(function (name, index) {
+ if (name  === "Waldo")
+ found(index)
+
+})
+
+}
+
+function actionWhenFound(index) {
+  console.log("Found Waldo at index " + index + "!");
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
